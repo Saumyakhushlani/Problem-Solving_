cff678368cbee5fbc5c63f8d980b08631c252a14
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-11-05 20:17:19 +0530

    04-11-2025

---
+++ Leetcode/445.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode* head= new ListNode(-1);
+        ListNode* temp= head;
+
+        int carry = 0;
+      
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            temp->next = new ListNode(sum);
+            temp = temp->next;
+        }
+        head = reverseList(head->next);
+        return head;
+    }
+};
